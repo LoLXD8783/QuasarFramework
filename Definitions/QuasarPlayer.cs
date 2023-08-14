a726cf6b88bf81aaca5144f1a43897c26fff9637
Aeterna <67712622+Aeterna145@users.noreply.github.com>
2023-08-14 00:49:35 -0400

    player org and update to hit shit

--- Definitions/QuasarPlayer.cs
+++ Definitions/QuasarPlayer.cs
@@ -83,7 +83,7 @@
             OverHealthDecay();
 
             if (healthCurrent <= 0 && overHealth <= 0 && Player.active && !Player.dead) //we check if active and NOT dead to make sure that KillMe() only runs once.
-                KillMe();
+                KillMe(); //you should LOVE yourself... NOW
 
             base.PostUpdate();
         }
@@ -152,6 +152,12 @@
     {
         public Dictionary<Material, int> materialInventory;
 
+        /// <summary>
+        /// Adds a given <see cref="Material"/> to the player's <see cref="materialInventory"/> Dictionary.
+        /// </summary>
+        /// <param name="player">The player this material is being given to.</param>
+        /// <param name="material">The material being given to this player.</param>
+        /// <param name="amount">The amount of the material being given.</param> //I ask that you don't judge this even though item.stack exists THANK YOU
         public void AddToMatInventory(QuasarPlayer player, Material material, int amount)
         {
             if (player.Player.whoAmI != Main.myPlayer)
@@ -182,10 +188,18 @@
         }
     }
 
-    //Respawn Handler
+    //Revive Handler
     partial class QuasarPlayer : ModPlayer
     {
         private int _respawnAnchorSetTimer = 60;
+
+        public bool canRevive;
+
+        public int reviveTimer;
+
+        public const int selfReviveTimer = 300;
+
+        public const int selfReviveTimerHard = 9000;
 
         public Vector2 respawnAnchorLastPos;
 
@@ -200,6 +214,15 @@
                 return currentPos;
             }
         }
+
+        public void ReviveMe()
+        {
+
+            OnRevive();
+        }
+
+        /// <summary> Allows you to make things happen when this player is revived. <para></para> Useful for resetting effects or states. </summary>
+        public virtual void OnRevive() { }
 
         public override void PreUpdateMovement()
         {
@@ -218,10 +241,12 @@
         }
     }
 
-    //Health / Resource Handler
-
+    //Health, Shields, Armor, Death, and Hits (it sounds like a lot but it's mostly connected here)
     partial class QuasarPlayer : ModPlayer
     {
+        /// <summary> Whether or not this player will use pre-determined hit effects. <para></para> Return false to write your own logic in <see cref="OnHit()"/> </summary>
+        public bool useVanillaHitEffects = true;
+
         /// <summary>
         /// Maximum => The maximum amount of health a player can have. <para></para>
         /// Current => The Current amount of health a player has in a given instance. <para></para>
@@ -231,8 +256,7 @@
 
         public int shieldsMaximum, shieldsCurrent;
 
-        
-
+        /// <summary> Automatically kills the player. </summary>
         public void KillMe()
         {
 
@@ -245,6 +269,10 @@
                 Player.dead = true;
 
                 //print death reason
+                // ||
+                // \/
+                //if we are in MP: print one for me, and one for the other players
+                //if we are in solo: print one for me.
 
                 //spawn respawn anchor
 
@@ -262,23 +290,69 @@
                 overHealth--;
         }
 
+        private void PostHitEffects() 
+        { 
+
+        }
+
+        /// <summary> Calculates the percentage damage reduction from the player's <see cref="armorTotal"/>. </summary>
+        /// <param name="totalArmor">The amount of armor the player has.</param>
+        /// <returns></returns>
+        private static float ArmorDamageReduction(int totalArmor)
+        {
+            float damageReduction = (float)(1 - (1 / ((0.17 * (totalArmor / 100)) + 1)));
+
+            //for every 100 armor, our resistance goes up by a given percentage. this is not linear, as it would easily outscale into an inconceivable percentage.
+            //thus, a hyperbolic model was chosen to better simulate an infinite scalar that will never reach 100%.
+
+            //for information of how this graph looks, visit https://www.desmos.com/calculator/es0rugng2v
+
+            //(it's really simple but the graph helps visualize!!!)
+
+            return damageReduction;
+        }
+
+        #region VIRTUALS
+
+        /// <summary> Allows you to make things happen when the player dies. </summary>
         public virtual void OnKill() { }
 
+        /// <summary> Allows you to make things happen after the player has already been hit. </summary>
+        public virtual void OnHit() { }
+
+        /// <summary> Allows you to make things happen after Overhealth is hit. <para></para> This method is only called if the hit is absorbed entirely by Overhealth. </summary>
+        public virtual void OnHitOverhealth() { }
+
+        /// <summary> Allows you to make things happen after Shields are hit. <para></para> This method is only called if the hit is absorbed entirely by Shields. </summary>
+        public virtual void OnHitShields() { }
+
+        /// <summary> Allows you to make things happen prior to the player being hit. </summary>
+        public virtual void PreHit() { }
+
+        #endregion
+
+        /// <summary> Detour of <see cref="Terraria.Player.Hurt(Player.HurtInfo, bool)"/> to redirect damage to our own health pool.</summary>
         private double ManualHurtDetour(On_Player.orig_Hurt_PlayerDeathReason_int_int_bool_bool_bool_int_bool_float orig, Player self, PlayerDeathReason damageSource, int Damage, int hitDirection, bool pvp, bool quiet, bool Crit, int cooldownCounter, bool dodgeable, float armorPenetration)
         {
-            if (overHealth > 0 )
+
+            PreHit();
+
+            if (overHealth > 0 ) //overhealth is first. hopefully you have any before getting hit! (adjust decay rate maybe?)
             {
                 if (overHealth > Damage)
-                    overHealth -= Damage;
+                    overHealth -= Damage; //overhealth is literally shields but they also decay
 
                 else
                 {
-                    Damage -= overHealth;
+                    Damage -= overHealth; //despite being called health, they don't take armor reduction
+
+                    OnHitOverhealth();
+
                     overHealth = 0;
                 }
             }
 
-            if (shieldsCurrent > 0)
+            if (shieldsCurrent > 0) //second is shields. these regen much faster than health (if your specialist has them or you build towards them)
             {
                 if (shieldsCurrent > Damage)
                     shieldsCurrent -= Damage;
@@ -286,18 +360,32 @@
                 else
                 {
                     Damage -= shieldsCurrent;
+
+                    OnHitShields();
+
                     shieldsCurrent = 0;
                 }
             }
 
-            if (healthCurrent > 0)
+            if (healthCurrent > 0) //health is the fun one.
             {
-                //armor calculations here
+                float damageReductionFromArmor = ArmorDamageReduction(armorTotal);
 
-                healthCurrent -= Damage;
+                Damage = (int)Math.Round(Damage * damageReductionFromArmor); //we ensure it stays an int (I wanted to use doubles but it's more annoying that way)
+
+                healthCurrent -= Damage; //since having damage equal to or greater than our health will kill us, we simply don't care about reduction after the fact.
             }
 
-            return orig.Invoke(self, damageSource, 0, 0, false, true, false, 0, false, 0.0f);
+            if (!useVanillaHitEffects)
+                OnHit();
+
+            else
+            {
+                PostHitEffects();
+                OnHit();
+            }
+
+            return orig.Invoke(self, damageSource, 0, 0, false, true, false, 0, false, 0.0f); //might remove this invocation.
         }
     }
 }
